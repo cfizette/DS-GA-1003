ca8d8913cf634e5a9add88ed72a9e88663b0280e
cfizette <cfizett1@binghamton.edu>
2019-04-02 20:48:04 -0400

    Initial commit of homework 5. Started some coding problems

---
+++ hw/5/logistic-code/logreg_skeleton.py
@@ -0,0 +1,31 @@
+import numpy as np
+def f_objective(theta, X, y, l2_param=1):
+    '''
+    Args:
+        theta: 1D numpy array of size num_features
+        X: 2D numpy array of size (num_instances, num_features)
+        y: 1D numpy array of size num_instances
+        l2_param: regularization parameter
+
+    Returns:
+        objective: scalar value of objective function
+    '''
+    n = len(y)
+    pred = X@theta
+    margin = -y*pred
+    log_loss = np.logaddexp(0, margin).sum()
+    reg = l2_param*(theta@theta)
+    return log_loss/n + reg
+    
+def fit_logistic_reg(X, y, objective_function, l2_param=1):
+    '''
+    Args:
+        X: 2D numpy array of size (num_instances, num_features)
+        y: 1D numpy array of size num_instances
+        objective_function: function returning the value of the objective
+        l2_param: regularization parameter
+        
+    Returns:
+        optimal_theta: 1D numpy array of size num_features
+    '''
+        
